b1c81650e68f19fe1b76979a2ee7011cf2c2c9dc
Brick Fiesta STEM <brickfiestastem@gmail.com>
2018-06-28 19:37:14 -0500

    Adding images and some layout.

---
+++ src/pages/scheduledetail/scheduledetail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ScheduledetailPage } from './scheduledetail';
+
+@NgModule({
+  declarations: [
+    ScheduledetailPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ScheduledetailPage),
+  ],
+})
+export class ScheduledetailPageModule {}
